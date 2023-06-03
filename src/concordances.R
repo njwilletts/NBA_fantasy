aa43a7dd005de2fbe8823f66edf52a3110befa77
njwilletts <106005317+njwilletts@users.noreply.github.com>
2023-06-03 14:56:53 +1200

    remove redundant import of fantasy teams

--- src/concordances.R
+++ src/concordances.R
@@ -3,9 +3,6 @@
 # Map team names to team codes
 team_codes <- read_excel(".\\data\\concordances.xlsx", sheet = "team_codes")
 
-# The NBA players in each fantasy team
-fantasy_teams <- read_excel(".\\data\\concordances.xlsx", sheet = "fantasy_teams")
-
 # Display name of each scraped data set
 data_names <- read_excel(".\\data\\concordances.xlsx", sheet = "data_names")
 
