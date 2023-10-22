33e0af137852f79602f04088f8e3c223a9af4c13
njwilletts <106005317+njwilletts@users.noreply.github.com>
2023-10-22 16:44:23 +1300

    add player_fix

--- src/concordances.R
+++ src/concordances.R
@@ -8,3 +8,6 @@ data_names <- read_excel(".\\data\\concordances.xlsx", sheet = "data_names")
 
 # Display name of each scraped schedule 
 schedule_names <- read_excel(".\\data\\concordances.xlsx", sheet = "schedule_names")
+
+# Fixes for player names
+player_fix <- read_excel(".\\data\\concordances.xlsx", sheet = "player_fix")
